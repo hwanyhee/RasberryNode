04242566d9a371e48a49723f049c7dd1b98388a4
CCH <hwanyhee@naver.com>
2018-09-09 13:51:26 +0900

    FGHF

--- PICamera_Nodejs.js
+++ PICamera_Nodejs.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 
 
 var app = express();
-app.set('port',process.env.PORT || 8080);
+app.set('port',process.env.PORT || 9090);
 
 app.set('views',path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
